1d7c25b11812db2151b68d4157038dd53162e038
Roman <dwerd191@gmail.com>
2021-02-11 21:44:33 +0500

    Изменили реализацию

--- src/main/java/com/myweb/controller/Controller.java
+++ src/main/java/com/myweb/controller/Controller.java
@@ -3,11 +3,14 @@ package com.myweb.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class Controller {
 
     @GetMapping("/")
-    public String getHelloWorld() {
-        return "Hello world!";
+    public List<String> getHelloWorld() {
+        return Arrays.asList("1","2","3");
     }
 }
